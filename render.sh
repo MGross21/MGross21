0fca76b4cda668369586dd75cd7d171b29d5c05a
MGross21 <mgstinger521@gmail.com>
2025-05-30 23:53:38 -0700

    Refine resume content: update skills section and enhance honors formatting

--- render.sh
+++ render.sh
@@ -12,3 +12,9 @@ latexmk -pdf -interaction=nonstopmode -halt-on-error -outdir=output cv.tex
 
 # Clean up auxiliary files
 latexmk -c -outdir=output
+
+# USAGE:
+# chmod +x render.sh
+# ./render.sh 
+# OR
+# ./render.sh >> /dev/null 2>&1 # **Silent**
